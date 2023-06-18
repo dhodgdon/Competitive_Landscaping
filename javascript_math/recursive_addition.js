aa8df52c907baa9df6743cd3d2beb7a382a958fd
dhodgdon <hod20010@byui.edu>
2023-06-17 23:41:51 -0600

    Tidied up the code.

--- javascript_math/recursive_addition.js
+++ javascript_math/recursive_addition.js
@@ -22,25 +22,3 @@ const odd_nums = nums.filter(num => num % 2 == 1)
 // Display the sums of our new arrays.
 console.log("Just the even numbers :", recursive_add(even_nums))
 console.log("Just the odd numbers :", recursive_add(odd_nums))
-
-
-
-// def fibonacci(n):
-//     if n == 0:
-//         return 0
-//     elif n == 1:
-//         return 1
-//     else:
-//         return fibonacci(n-1) + fibonacci(n-2)
-
-// def recursive_addition(x, y):
-//     if y == 0:
-//         return x
-//     return recursive_addition(x+1, y-1)
-
-
-// def recursive_add(numbers):
-//     if len(numbers) == 1:
-//         return numbers[0]
-//     else:
-//         return numbers[0] + recursive_add(numbers[1:])
